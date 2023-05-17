8bc924a1962edad8246ffc71b94008661fc486c4
joraff <joraff@gmail.com>
2023-05-17 17:07:14 -0500

    Remove bucket ACL
    Now private by default: https://aws.amazon.com/blogs/aws/heads-up-amazon-s3-security-changes-are-coming-in-april-of-2023/

--- main.tf
+++ main.tf
@@ -5,10 +5,10 @@ resource "aws_s3_bucket" "state" {
   bucket = "terraform-state-${data.aws_caller_identity.current.account_id}"
 }
 
-resource "aws_s3_bucket_acl" "state" {
-  bucket = aws_s3_bucket.state.id
-  acl    = "private"
-}
+# resource "aws_s3_bucket_acl" "state" {
+#   bucket = aws_s3_bucket.state.id
+#   acl    = "private"
+# }
 
 resource "aws_s3_bucket_versioning" "state" {
   bucket  = aws_s3_bucket.state.id
@@ -17,13 +17,13 @@ resource "aws_s3_bucket_versioning" "state" {
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "state" {
-  bucket                  = aws_s3_bucket.state.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
+# resource "aws_s3_bucket_public_access_block" "state" {
+#   bucket                  = aws_s3_bucket.state.id
+#   block_public_acls       = true
+#   block_public_policy     = true
+#   ignore_public_acls      = true
+#   restrict_public_buckets = true
+# }
 
 resource "aws_dynamodb_table" "state" {
   name         = "terraform-state-${data.aws_caller_identity.current.account_id}"
