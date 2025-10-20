66c1e6ed06075a91b6e4ef88c940b428163fd891
Joseph Rafferty <jrafferty@tamu.edu>
2025-10-20 15:59:38 -0500

    makes dynamodb optional

--- main.tf
+++ main.tf
@@ -4,7 +4,9 @@ data "aws_region" "current" {}
 locals {
   bucket_name         = var.bucket_name != null ? var.bucket_name : "terraform-state-${data.aws_caller_identity.current.account_id}"
   dynamodb_table_name = var.dynamodb_table_name != null ? var.dynamodb_table_name : "terraform-state-${data.aws_caller_identity.current.account_id}"
+  use_s3_locking      = var.dynamodb_table_name == null
 }
+
 resource "aws_s3_bucket" "state" {
   bucket = local.bucket_name
 }
@@ -17,6 +19,8 @@ resource "aws_s3_bucket_versioning" "state" {
 }
 
 resource "aws_dynamodb_table" "state" {
+  count = var.use_dynamodb ? 1 : 0
+
   name         = local.dynamodb_table_name
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
@@ -39,5 +43,7 @@ output "bucket" {
 }
 
 output "dynamodb_table" {
-  value = aws_dynamodb_table.state.id
+  value = var.use_dynamodb ? aws_dynamodb_table.state[0].name : null
+}
+
 }
