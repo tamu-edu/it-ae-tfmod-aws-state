e356cab4e261427c9c7f4f939a5a848c28c21834
Joseph Rafferty <jrafferty@tamu.edu>
2025-10-20 16:18:41 -0500

    adds required provider

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0"
+    }
+  }
+}
+
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
